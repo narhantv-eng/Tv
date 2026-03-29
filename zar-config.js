e9437875860d8ec227faa4c7ec69591d62d722c8
narhantv-eng <narhantv@gmail.com>
2026-03-29 20:58:52 +0800

    Update and rename zar.js to zar-config.js

---
+++ zar-config.js
@@ -0,0 +1,11 @@
+// zar-config.js
+window.MY_ADS =[
+  { afterRowId: 'rowFeatured',   isActive: true,  label: 'BANNER 1' },
+  { afterRowId: 'rowSeries',     isActive: true,  label: 'BANNER 2' },
+  { afterRowId: 'rowAction',     isActive: true,  label: 'BANNER 3' },
+  { afterRowId: 'rowDrama',      isActive: false, label: 'BANNER 4' },
+  { afterRowId: 'rowComedy',     isActive: false, label: 'BANNER 5' },
+  { afterRowId: 'gameRow_puzzle',   isActive: true, label: 'GAME AD 1' },
+  { afterRowId: 'gameRow_strategy', isActive: true, label: 'GAME AD 2' },
+  { afterRowId: 'gameRow_arcade',   isActive: true, label: 'GAME AD 3' }
+];
